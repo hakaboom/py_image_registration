e5f9c37f2b09ab80d5c03f565a36c4a0cd3658d2
huangziqing <270704881@qq.com>
2021-06-16 17:59:18 +0800

    1.0.1

---
+++ main.py
@@ -0,0 +1,4 @@
+"""
+python setup.py sdist
+twine upload dist/*
+"""
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='image_registration',
-    version='1.0.0',
+    name='py-image-registration',
+    version='1.0.1',
     author='hakaboom',
+    license="Apache License 2.0",
     author_email='1534225986@qq.com',
     description='image registration by ORB/SIFT/SURF/CUDA_SURF/ORB/CUDA_ORB/matchTemplate',
     url='https://github.com/hakaboom/py_image_registration',
-    packages=['baseImage'],
+    packages=['image_registration'],
     install_requires=['colorama>=0.4.4',
                       "loguru>=0.5.3",
 ],
